282430cd2bfa4334cf0becf20de826d8311ca30e
Alina <alina12485615.we@gmail.com>
2023-04-15 11:43:48 +0700

    startGitProject

---
+++ app/src/main/java/com/example/pr7_zlobina_pr21101/Contacts.java
@@ -0,0 +1,14 @@
+package com.example.pr7_zlobina_pr21101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Contacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacts);
+    }
+}
